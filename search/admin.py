919328465828666099a081ae20fab88f561b1a04
Yu-Hsin Lin <charlie881007@gmail.com>
2020-05-09 13:36:20 +0800

    add admin page

--- search/admin.py
+++ search/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+admin.site.register(models.Courses1081)
+admin.site.register(models.Coursesreport1081x1)
+admin.site.register(models.Coursesreport1081x2)
+admin.site.register(models.Courses1082)
+admin.site.register(models.Coursesreport1082x1)
+admin.site.register(models.Coursesreport1082x2)
